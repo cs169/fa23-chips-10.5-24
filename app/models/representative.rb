f162c2e7b6161024c363c4a258faffc3cb6080c0
WAAutoMaton <a547673135@gmail.com>
2023-11-21 04:54:53 +0000

    [feat] Implement  profile page: Implement representatives#show; Migrate database; Link from representatives#search

--- app/models/representative.rb
+++ app/models/representative.rb
@@ -16,9 +16,17 @@ class Representative < ApplicationRecord
           ocdid_temp = office.division_id
         end
       end
+      address_line1 = official.address ? official.address[0].line1 : nil
+      address_line2 = official.address ? official.address[0].line2 : nil
+      address_city = official.address ? official.address[0].city : nil
+      address_state = official.address ? official.address[0].state : nil
+      address_zip = official.address ? official.address[0].zip : nil
 
       rep = Representative.create!({ name: official.name, ocdid: ocdid_temp,
-          title: title_temp })
+        title: title_temp, 
+        address_line1: address_line1, address_line2: address_line2, 
+        address_city: address_city, address_state: address_state,
+        address_zip: address_zip, party: official.party, photo_url: official.photo_url})
       reps.push(rep)
     end
 
