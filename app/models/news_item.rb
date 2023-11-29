590efe11a190a90017aa2c9fba80bd818fdead9e
WAAutoMaton <a547673135@gmail.com>
2023-11-29 08:00:51 +0000

    [feat] Add news_item.rating

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -3,10 +3,19 @@
 class NewsItem < ApplicationRecord
   belongs_to :representative
   has_many :ratings, dependent: :delete_all
+  attribute :ratings_sum, :integer, default: 0
 
   def self.find_for(representative_id)
     NewsItem.find_by(
       representative_id: representative_id
     )
+  end
+
+  # Return the average rating for this news item.
+  # If there are no ratings, return nil.
+  def rating
+    return nil if ratings.size.zero?
+
+    ratings_sum / ratings.size.to_f
   end
 end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -3,4 +3,16 @@
 class Rating < ApplicationRecord
   belongs_to :user
   belongs_to :news_item
+
+  validates! :news_item, presence: true
+
+  after_create :update_news_item_ratings_sum
+
+  private
+
+  def update_news_item_ratings_sum
+    news_item.update(ratings_sum: news_item.ratings_sum + score)
+    news_item.ratings << self
+    news_item.save!
+  end
 end
